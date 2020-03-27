4a65a2d5aa3c0c6383da5f69dff86d6c673b73d7
junaid <junaidatique@gmail.com>
2020-03-27 18:21:14 +0500

    Adding withKnobs addon.

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,5 +2,10 @@
 
 module.exports = {
   stories: ['../src/components/**/*.stories.js'],
-  addons: ['@storybook/preset-create-react-app', '@storybook/addon-actions', '@storybook/addon-links'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-knobs'
+  ],
 };
